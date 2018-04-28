e5b6e18b023a2486209b5394bd2f4500c9c01c5d
Nirmoho Banerjee <nban12345@gmail.com>
2018-04-27 20:18:30 -0400

    Implemented Maze Read operations

--- ValueIteration/src/StateParameters.java
+++ ValueIteration/src/StateParameters.java
@@ -1,11 +1,206 @@
 /**
- * 
- */
-
-/**
  * @author nirmoho-Mac
+ * 
+ * Class to Store State Variables
  *
  */
 public class StateParameters {
+	
+	/**
+	 * Private variable hold x coordinate of state. 
+	 */
+	private int xCoord;
+	/**
+	 * Private variable hold y coordinate of state. 
+	 */
+	private int yCoord;
+	/**
+	 * Array to hold q values for a particular state
+	 */
+	private double[] qValues;
+	/**
+	 * Variable hold V*(s) for the state
+	 */
+	private double value;
+	/**
+	 * Variable to hold pi*(s) for the state
+	 */
+	private double policy;
+	/**
+	 * Variable to hold maze value
+	 */
+	private char cellVal;
+	/**
+	 * Terminal Flag.
+	 */
+	private boolean isTerminal;
+	/**
+	 * Obstacle Flag.
+	 */
+	private boolean isObstacle;
+	/**
+	 * Start cell flag
+	 */
+	private boolean isStartFlag;
+	/**
+	 * Default Constructor.
+	 */
+	public StateParameters()	{
+		this.xCoord = 0;
+		this.yCoord = 0;
+		this.value = 0;
+		this.policy = 0;
+	}
+	/**
+	 * Parameterized constructor.
+	 * @param xCoord - of state
+	 * @param yCoord - of state
+	 * @param actionList - List of all possible actions.
+	 */
+	public StateParameters(int xCoord, int yCoord, int[] actionList, char cell) {
+		super();
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.value = 0;
+		this.policy = 0;
+		this.qValues = new double[actionList.length];
+		this.cellVal = cell;
+	}
 
+	/**
+	 * Getter for xCoord.
+	 * @return xCoord
+	 */
+	public int getxCoord() {
+		return xCoord;
+	}
+	/**
+	 * Setter for xCoord.
+	 * @param xCoord
+	 */
+	public void setxCoord(int xCoord) {
+		this.xCoord = xCoord;
+	}
+	/**
+	 * Getter for yCoord.
+	 * @return yCoord
+	 */
+	public int getyCoord() {
+		return yCoord;
+	}
+	/**
+	 * Setter for yCoord
+	 * @param yCoord
+	 */
+	public void setyCoord(int yCoord) {
+		this.yCoord = yCoord;
+	}
+	/**
+	 * Getter for qValues.
+	 * @return
+	 */
+	public double[] getqValues() {
+		double[] retArray = new double[this.qValues.length];
+		System.arraycopy(this.qValues, 0, retArray, 0, this.qValues.length);
+		return retArray;
+	}
+	/**
+	 * Setter for array
+	 * @param qValues
+	 */
+	public void setqValues(double[] qValues) {
+		this.qValues = qValues;
+	}
+	/**
+	 * Getter for optimal Value of State
+	 * @return
+	 */
+	public double getValue() {
+		return value;
+	}
+	/**
+	 * Setter for optimal Value
+	 * @param value
+	 */
+	public void setValue(double value) {
+		this.value = value;
+	}
+	/**
+	 * Getter for optimal policy.
+	 * @return policy
+	 */
+	public double getPolicy() {
+		return policy;
+	}
+	/**
+	 * Setter for optimal policy.
+	 * @param policy
+	 */
+	public void setPolicy(double policy) {
+		this.policy = policy;
+	}
+	@Override
+	public String toString()	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		sb.append("Value = " + this.cellVal);
+		sb.append(" ]");
+		return sb.toString();
+	}
+	/**
+	 * Get Value in maze.
+	 * @return
+	 */
+	public char getCellVal() {
+		return cellVal;
+	}
+	/**
+	 * set cell value.
+	 * @param cellVal
+	 */
+	public void setCellVal(char cellVal) {
+		this.cellVal = cellVal;
+	}
+	/**
+	 * is Terminal Cell getter.
+	 * @return true/false
+	 */
+	public boolean isTerminal() {
+		return isTerminal;
+	}
+	/**
+	 * setTerminxal setter.
+	 * @param isTerminal
+	 */
+	public void setTerminal(boolean isTerminal) {
+		this.isTerminal = isTerminal;
+	}
+	/**
+	 * is Obstacle getter
+	 * @return
+	 */
+	public boolean isObstacle() {
+		return isObstacle;
+	}
+	/**
+	 * setter for obstacle
+	 * @param isObstacle
+	 */
+	public void setObstacle(boolean isObstacle) {
+		this.isObstacle = isObstacle;
+	}
+	/**
+	 * check if start terminal.
+	 * @return
+	 */
+	public boolean isStartFlag() {
+		return isStartFlag;
+	}
+	/**
+	 * set start flag.
+	 * @param isStartFlag
+	 */
+	public void setStartFlag(boolean isStartFlag) {
+		this.isStartFlag = isStartFlag;
+	}
 }
--- ValueIteration/src/value_iteration.java
+++ ValueIteration/src/value_iteration.java
@@ -1,19 +1,66 @@
 /**
- * 
- */
-
-/**
  * @author nirmoho-Mac
  *
  */
-public class value_iteration {
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class value_iteration {
 	/**
+	 * Method to read input file
+	 * @param fileName
+	 * @return
+	 */
+	public static StateParameters[][] readMaze(String fileName, int[] actionsList) throws IOException	{
+		File file = new File(fileName);
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(file);
+		int lineCount = 0;
+		String line = "";
+		while(scanner.hasNextLine())	{
+			lineCount++;
+			line = scanner.nextLine();
+		}
+		scanner.close();
+		StateParameters[][] maze = new StateParameters[lineCount][line.length()];
+		scanner = new Scanner(file);
+		int i = 0;
+		while(scanner.hasNextLine())	{
+			line = scanner.nextLine();
+			for (int j = 0; j < line.length(); j++)	{
+				char ch = line.charAt(j);
+				maze[i][j] = new StateParameters(i, j, actionsList, ch);
+				if (ch == '*')	{
+					maze[i][j].setObstacle(true);
+				}
+				maze[i][j].setStartFlag(false);
+				maze[i][j].setTerminal(false);
+			}
+			i++;
+		}
+		maze[lineCount - 1][0].setStartFlag(true);
+		maze[0][lineCount - 1].setTerminal(true);
+		scanner.close();
+		return maze;
+	}
+	/**
+	 * Command Line Arguments.
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		//Get all command line arguments
+		String mazeFile = args[0];
+		String valueFile = args[1];
+		String qFile = args[2];
+		String policyFile = args[3];
+		int numEpoch = Integer.parseInt(args[4]);
+		double gamma = Double.parseDouble(args[5]);
+		
+		int[] actionsList = {0, 1, 2, 3};
+		StateParameters[][] mazeDS = readMaze(mazeFile, actionsList);
 	}
 
 }
